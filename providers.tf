6e5ca2a7907bdab0b93e8e4fb8f53db7010163fe
TQay <46206530+TQay@users.noreply.github.com>
2024-01-21 17:16:12 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,6 +8,8 @@ terraform {
 }
 
 provider "aws" {
+  access_key = "AKIA3TF6W6XRIGEBGNML"
+  secret_key = "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"
   region = "us-east-1" 
   profile = "tfadmin"
 }
